3a9dbe95445d43889895d2770b4ee11c87b58c18
jmgonzalez4 <jmgonzalez4@uc.cl>
2018-11-15 22:34:04 -0300

    Se actualizaron los .c de implicit y explicit con los comentarios pertinentes

--- Tarea 3/explicit.c
+++ Tarea 3/explicit.c
@@ -1,48 +1,119 @@
-/*  Facultad de Física de la Pontificia Universidad Católica de Chile
-    Física Computacional (FIZ1431) - Segundo Semestre de 2018
+/*  FACULTAD DE FÍSICA DE LA PONTIFICIA UNIVERSIDAD CATÓLICA DE CHILE
+    FÍSICA COMPUTACIONAL (FIZ1431) - SEGUNDO SEMESTRE DE 2018
 
-    Alumnos:  Nicolás Van Sint Jan Campos (nicovsj@uc.cl)
-              Juan Manuel González Brantes (jmgonzalez4@uc.cl)
+    ALUMNOS:  NICOLÁS ANDRÉ VAN SINT JAN CAMPOS (nicovsj@uc.cl)
+              JUAN MANUEL GONZÁLEZ BRANTES (jmgonzalez4@uc.cl)
 
-    Profesor: Edgardo Dörner Yaksic (endorner@fis.uc.cl)
+    PROFESOR: EDGARDO ANDRÉS DÖRNER YAKSIC (endorner@fis.uc.cl)
 
-    --- TAREA 3: . ---
+    FECHA LÍMITE DE ENTREGA: JUEVES 15 DE NOVIEMBRE DE 2018 A LAS 23:59 HORAS
 
-    Enunciado: 
+-------------------------------------------------------------------------------
+              TAREA 3 - LA ECUACIÓN DEL CALOR DEPENDIENTE DEL TIEMPO
 
-    Objetivos propuestos: 
+    Calcule la evolución temporal de la distribución de temperatura T(x,t) a
+    través de una barra cilíndrica, que está expuesta a un sumidero de calor
+    alrededor de x = L/2. Este sumidero de calor está descrito por una función
+    Gamma(x) de forma gaussiana. La barra cilíndrica está expuesta a temperaturas
+    constantes T_0 Y T_N en sus extremos. Utilice los siguientes parámetros:
 
-    BIBLIOGRAFÍA: Libro guía del curso
-    Stickler, Benjamin and Schachinger, Ewald: "Basic Concepts in Computational
-    Physics", Editorial Springer Verlag, Ebook de 2014. */
+    L = 10; k = 1; Theta (mayúscula) = -0.4; l = 1; T_0 = 0; T_N = 2; dt = 0.5
 
+    Adicionalmente, para la condición inicial considere T(x,0) = 0, x \in [0, L].
+    Puede obtener gráficos para diferentes pasos temporales, o bien, una simula-
+    ción continua (animación) de hasta 300 pasos.
+
+    Para resolver el problema, debe utilizar un método explícito y un método
+    implícito de resolución numérica, según lo visto en clases. Justifique ade-
+    cuadamente su elección. Por supuesto, no puede emplear ni el método de Euler
+    implícito como el explícito. Una resolución implícita implica resolver un
+    sistema de ecuaciones lineales. Para esto, debe buscar una librería o código
+    externo que le permita resolver el problema. Una buena referencia es:
+
+    Press, William., et. al., "Numerical Recipes in C" (Cambridge University Press,
+    First Edition, 1988)
+
+    Sea cual sea el método escogido, debe referenciarlo correctamente, indicando
+    las ventajas y desventajas en función del problema numérico que está intentan
+    do resolver. No puede utilizar el código visto en clases para este propósito.
+
+--------------------------------------------------------------------------------
+
+    OBJETIVOS PROPUESTOS:
+    -Resolver, tanto mediante condiciones de borde definidas como parámetros fí-
+    sicos ya indicados, la ecuación del calor dependiente del tiempo para una
+    barra cilíndrica, utilizando para estos efectos la física computacional. En
+    particular, con métodos numéricos de programación implícito y explícito.
+    -Utilizar, de manera óptima, métodos numéricos de programación implícitos y
+    explícitos para el propósito anterior; la idea será no reciclar el código
+    exhibido en clases, sino, la implementación de un código propio que incluya
+    esta teoría.
+    -Evidenciar, por medio de animación computacional, la evolución temporal que
+    sufre la barra cilíndrica en cuestión, tal que permita establecer una visua-
+    lización sobre la fenomenología del problema físico.
+    -Referenciar e indicar, mediante la bibliografía apropiada, la implementación
+    del código en virtud de sus ventajas y desventajas.
+
+--------------------------------------------------------------------------------
+
+    REFERENCIAS:
+    [1] Stickler, Benjamin; Schachinger, Ewald: "Basic Concepts in Computational
+    Physics". Primera Edición, Editorial Springer-Verlag, 2014.
+    [2] Press, William H., et. al.; “Numerical recipes in C.”. Primera Edición,
+    Cambridge University Press, 1988.
+    [3] Recktenwald, Gerald: Crank-Nicholson Solution to the Heat Equation:
+    ME 448/548 Notes. Portland State University; Mechanical Engineering Department.
+    http://web.cecs.pdx.edu/~gerry/class/ME448/notes/pdf/CN_slides.pdf (visitado
+    el 15 de noviembre de 2018).
+    [4] Dörner, Edgardo. "Clase 11: ODE Valores Iniciales". Diapositivas del curso
+    Física Computacional, Segundo Semestre de 2018, Pontificia Universidad Católica
+    de Chile.
+    [5] Dörner, Edgardo. "Clase 18: Ecuación de Calor 1D". Diapositivas del curso
+    Física Computacional, Segundo Semestre de 2018, Pontificia Universidad Católica
+    de Chile.
+
+--------------------------------------------------------------------------------
+
+    A continuación, se presenta el código que alberga un método explícito de re-
+    solución numérica para el problema de ecuación de calor en una barra cilíndrica.
+    En este caso, es decir, en la resolución del problema mediante el método explí-
+    cito, no se requirió de búsqueda de librerías o de una decisión más pensada para
+    establecer el código, pues ya se contaba con el método de Runge-Kutta explícito
+    para poder resolver el problema.
+
+    Nuevamente, esta elección tuvo un contacto directo con lo visto en clases, pues
+    de éstas, tanto por el sustento teórico como por la práctica misma de ejercicios
+    en clases, ya era sabido que el método explícito garantizaba efectividad para el
+    problema presente acá.
+
+*/
 // ----------------------------  INICIO TAREA  ------------------------------ //
 
-
+// Importación de las librerías necesarias para la compilación correcta de los
+// módulos que contienen y que se utilizarán.
 #include <stdio.h>
 #include <stdlib.h>
 #include <math.h>
 
-// Constantes del material
-const double L = 10.0;                      // Largo del cilíndro
-const double kappa = 1.0;                   // Constante
+// Constantes del material que constituye la barra cilíndrica.
+const double L = 10.0;                      // Longitud del cilindro
+const double kappa = 1.0;                   // Constante de conductividad térmica
 
-// Condiciones de borde
-const double T0 = 0.0;                      // Temperatura T(0, t)
-const double Tf  = 2.0;                     // Temperatura T(N, t)
+// Establecimiento de condiciones de borde para el problema.
+const double T0 = 0.0;                      // Temperatura en x = 0; T(0,t)
+const double Tf  = 2.0;                     // Temperatura en x = N; T(N,t)
 
-// Constantes del sumidero
+// Constantes del sumidero que afecta a la barra cilíndrica.
 const double Theta = -0.4;
 const double l = 1.0;
 
-// Elección de discretizaciones para resolver 
-// numéricamente
-const int nnodes = 60;
-                    // Número de pasos
-const double dx = L / (nnodes-1);                      // Ancho espacial (debe ser >= 1.0)
-const double dt = dx * dx / (kappa * 2.0);                      // Ancho temporal
-const int nsteps = 1200; 
+// Discretización para poder resolver numéricamente.
+const int nnodes = 60;                        // Número de pasos temporales
+const double dx = L / (nnodes-1);             // Ancho espacial (debe ser >= 1.0)
+const double dt = dx * dx / (kappa * 2.0);    // Ancho temporal
+const int nsteps = 1200;                      // Número de pasos espaciales
 
+// Definición computacional del sumidero de calor.
 double Gamma(double number);
 void F(double* Yin, double* Yout, int dim);
 void eRK4(double* Yin, double* Yout, double dt, int dim);
@@ -61,19 +132,20 @@ int main(int argc, const char * argv[]) {
         u[0][i] = 0.0;
     }
 
-    // Condición de borde (x = 0; x = N)
+    // Condiciones de borde (x = 0; x = N)
     for (int i = 0; i < nsteps; ++i) {
         u[i][0] = T0;
         u[i][nnodes-1] = Tf;
     }
 
-    // EXPLICIT RUNGE-KUTTA
+    // IMPLEMENTACIÓN DEL MÉTODO RUNGE-KUTTA EXPLÍCITO
 
     for (int t=0; t<nsteps-1; t++) {
       eRK4(u[t], u[t+1], dt, nnodes);
     }
 
-    // Guardamos los resultados en un archivo para su analisis posterior.
+    // Registro y retención de los datos obtenidos mediante el método numérico
+    // para el posterior análisis y uso gráfico animado.
     FILE *fp = fopen("res_explicit.csv", "w");
 
     fprintf(fp, "t,");
@@ -82,7 +154,7 @@ int main(int argc, const char * argv[]) {
     }
     fprintf(fp, "x=%2.3f\n", dx*(nnodes-1));
 
-    
+
     for (int t=0; t<nsteps; t++) {
 
         fprintf(fp, "%3d,", t);
@@ -94,33 +166,34 @@ int main(int argc, const char * argv[]) {
     }
     fclose(fp);
 
-    // Imprimimos los valores finales como muestra.
+    // Exhibición de los valores finales como muestra de la efectividad del código
     printf("Simulación terminada exitosamente\n");
-    
+
     return 0;
 }
 
-
+// Función sumidero-fuente en L/2 para la barra del calor descrita por la ecuación
+// del calor no homogénea.
 double Gamma(double number) {
-  /* Sumidero/fuente en L/2 para la ecuación de calor inhomogénea */
   return Theta / l * exp(-1.0 * pow((number - L/2)/l, 2));
 }
 
 void F(double* Yin, double* Yout, int dim) {
-    Yout[0] = 0.0; Yout[dim-1] = 0.0;  // Truco para que las condiciones de borde funcionen
+    Yout[0] = 0.0; Yout[dim-1] = 0.0;     // Truco para que las condiciones de borde funcionen
     for (int i = 1; i < dim-1; ++i) {
         Yout[i] = kappa/(dx*dx) * (Yin[i-1] - 2*Yin[i] + Yin[i+1]) - Gamma(i*dx);
     }
 }
 
+// Inicio - Método eRK-4
 void eRK4(double* Yin, double* Yout, double dt, int dim) {
-    /* Implementación del Método Runge-Kutta a orden 4
 
-    Args:
+    /*
+    Argumentos necesarios:
         - double* Yin: Arreglo input a realizar (Y_n)
         - double* Yout: Arreglo output esperado luego de eRK4 (Y_(n+1))
         - double dt: Paso temporal
-        - double dim: Número de entradas que posee Y_n 
+        - double dim: Número de entradas que posee Y_n
     */
 
   double Y1[dim], Y2[dim], Y3[dim], Y4[dim];
--- Tarea 3/implicit.c
+++ Tarea 3/implicit.c
@@ -1,48 +1,129 @@
-/*  Facultad de Física de la Pontificia Universidad Católica de Chile
-    Física Computacional (FIZ1431) - Segundo Semestre de 2018
+/*  FACULTAD DE FÍSICA DE LA PONTIFICIA UNIVERSIDAD CATÓLICA DE CHILE
+    FÍSICA COMPUTACIONAL (FIZ1431) - SEGUNDO SEMESTRE DE 2018
 
-    Alumnos:  Nicolás Van Sint Jan Campos (nicovsj@uc.cl)
-              Juan Manuel González Brantes (jmgonzalez4@uc.cl)
+    ALUMNOS:  NICOLÁS ANDRÉ VAN SINT JAN CAMPOS (nicovsj@uc.cl)
+              JUAN MANUEL GONZÁLEZ BRANTES (jmgonzalez4@uc.cl)
 
-    Profesor: Edgardo Dörner Yaksic (endorner@fis.uc.cl)
+    PROFESOR: EDGARDO ANDRÉS DÖRNER YAKSIC (endorner@fis.uc.cl)
 
-    --- TAREA 3: . ---
+    FECHA LÍMITE DE ENTREGA: JUEVES 15 DE NOVIEMBRE DE 2018 A LAS 23:59 HORAS
 
-    Enunciado: 
+-------------------------------------------------------------------------------
+              TAREA 3 - LA ECUACIÓN DEL CALOR DEPENDIENTE DEL TIEMPO
 
-    Objetivos propuestos: 
+    Calcule la evolución temporal de la distribución de temperatura T(x,t) a
+    través de una barra cilíndrica, que está expuesta a un sumidero de calor
+    alrededor de x = L/2. Este sumidero de calor está descrito por una función
+    Gamma(x) de forma gaussiana. La barra cilíndrica está expuesta a temperaturas
+    constantes T_0 Y T_N en sus extremos. Utilice los siguientes parámetros:
 
-    BIBLIOGRAFÍA: Libro guía del curso
-    Stickler, Benjamin and Schachinger, Ewald: "Basic Concepts in Computational
-    Physics", Editorial Springer Verlag, Ebook de 2014. */
+    L = 10; k = 1; Theta (mayúscula) = -0.4; l = 1; T_0 = 0; T_N = 2; dt = 0.5
 
+    Adicionalmente, para la condición inicial considere T(x,0) = 0, x \in [0, L].
+    Puede obtener gráficos para diferentes pasos temporales, o bien, una simula-
+    ción continua (animación) de hasta 300 pasos.
+
+    Para resolver el problema, debe utilizar un método explícito y un método
+    implícito de resolución numérica, según lo visto en clases. Justifique ade-
+    cuadamente su elección. Por supuesto, no puede emplear ni el método de Euler
+    implícito como el explícito. Una resolución implícita implica resolver un
+    sistema de ecuaciones lineales. Para esto, debe buscar una librería o código
+    externo que le permita resolver el problema. Una buena referencia es:
+
+    Press, William., et. al., "Numerical Recipes in C" (Cambridge University Press,
+    First Edition, 1988)
+
+    Sea cual sea el método escogido, debe referenciarlo correctamente, indicando
+    las ventajas y desventajas en función del problema numérico que está intentan
+    do resolver. No puede utilizar el código visto en clases para este propósito.
+
+--------------------------------------------------------------------------------
+
+    OBJETIVOS PROPUESTOS:
+    -Resolver, tanto mediante condiciones de borde definidas como parámetros fí-
+    sicos ya indicados, la ecuación del calor dependiente del tiempo para una
+    barra cilíndrica, utilizando para estos efectos la física computacional. En
+    particular, con métodos numéricos de programación implícito y explícito.
+    -Utilizar, de manera óptima, métodos numéricos de programación implícitos y
+    explícitos para el propósito anterior; la idea será no reciclar el código
+    exhibido en clases, sino, la implementación de un código propio que incluya
+    esta teoría.
+    -Evidenciar, por medio de animación computacional, la evolución temporal que
+    sufre la barra cilíndrica en cuestión, tal que permita establecer una visua-
+    lización sobre la fenomenología del problema físico.
+    -Referenciar e indicar, mediante la bibliografía apropiada, la implementación
+    del código en virtud de sus ventajas y desventajas.
+
+--------------------------------------------------------------------------------
+
+    REFERENCIAS:
+    [1] Stickler, Benjamin; Schachinger, Ewald: "Basic Concepts in Computational
+    Physics". Primera Edición, Editorial Springer-Verlag, 2014.
+    [2] Press, William H., et. al.; “Numerical recipes in C.”. Primera Edición,
+    Cambridge University Press, 1988.
+    [3] Recktenwald, Gerald: Crank-Nicholson Solution to the Heat Equation:
+    ME 448/548 Notes. Portland State University; Mechanical Engineering Department.
+    http://web.cecs.pdx.edu/~gerry/class/ME448/notes/pdf/CN_slides.pdf (visitado
+    el 15 de noviembre de 2018).
+    [4] Dörner, Edgardo. "Clase 11: ODE Valores Iniciales". Diapositivas del curso
+    Física Computacional, Segundo Semestre de 2018, Pontificia Universidad Católica
+    de Chile.
+    [5] Dörner, Edgardo. "Clase 18: Ecuación de Calor 1D". Diapositivas del curso
+    Física Computacional, Segundo Semestre de 2018, Pontificia Universidad Católica
+    de Chile.
+
+--------------------------------------------------------------------------------
+
+    A continuación, se presenta un código que alberga un método implícito de re-
+    solución numérica para el problema de ecuación de calor en una barra cilín-
+    drica. En primer lugar, se buscó en [1], el libro guía del curso, alguna in-
+    dicación que pudiera servir, terminando este libro por referenciar a [2]. En
+    [2], se encontró un método de tridiagonalización en el capítulo 3 de sistemas
+    de ecuaciones lineales, el que permitió implementar la matriz tridiagonal,
+    para, posteriormente, encontrar [3] y decidir finalmente, que el método Crank-
+    Nicolson era el indicado para resolver implícitamente la ecuación del calor.
+
+    Esta elección tuvo un asidero teórico visto en clases, pues, en [4], se obser-
+    vó que es un método implícito, para el que, en [5], existía un argumento muy
+    contundente que permitió finalmente decidir usarlo: si bien es un método implí
+    cito, es incondicionalmente estable, queriendo decir que, sobre la base de ser
+    predictor-corrector, no es alterado por condiciones externas lo suficientemente
+    desestabilizadoras.
+
+    No obstante, este método cuenta con la desventaja de que, como ya se señaló,
+    es implícito, es decir, su solución no es hallada de forma directa: de ahí,
+    la dificultad de implementar el código (de más abajo) que define la matriz
+    tridiagonal junto a sus vectores, el vector solución, las condiciones de borde,
+    y los índices que definen las posiciones de los inputs.
+
+ */
 // ----------------------------  INICIO TAREA  ------------------------------ //
 
-
+// Importación de las librerías necesarias para la compilación correcta de los
+// módulos que contienen y que se utilizarán.
 #include <stdio.h>
 #include <stdlib.h>
 #include <math.h>
 
-// Constantes del material
-const double L = 10.0;                      // Largo del cilíndro
-const double kappa = 1.0;                   // Constante
+// Constantes del material que constituye la barra cilíndrica.
+const double L = 10.0;                      // Longitud del cilindro.
+const double kappa = 1.0;                   // Constante de conductividad térmica
 
-// Condiciones de borde
-const double T0 = 0.0;                      // Temperatura T(0, t)
-const double Tf  = 2.0;                     // Temperatura T(N, t)
+// Establecimiento de condiciones de borde para el problema.
+const double T0 = 0.0;                      // Temperatura en x = 0; T(0,t)
+const double Tf  = 2.0;                     // Temperatura en x = N; T(N,t)
 
-// Constantes del sumidero
+// Constantes del sumidero que afecta a la barra cilíndrica.
 const double Theta = -0.4;
 const double l = 1.0;
 
-// Elección de discretizaciones para resolver 
-// numéricamente
-const int nnodes = 60;
-                    // Número de pasos
-const double dx = L / (nnodes-1);                      // Ancho espacial (debe ser >= 1.0)
-const double dt = dx * dx / (kappa * 2.0) + 0.1;                      // Ancho temporal
-const int nsteps = 1200; 
+// Discretización para poder resolver numéricamente.
+const int nnodes = 60;                              // Número de pasos temporales
+const double dx = L / (nnodes-1);                   // Ancho espacial (debe ser >= 1.0)
+const double dt = dx * dx / (kappa * 2.0) + 0.1;    // Ancho temporal
+const int nsteps = 1200;                            // Número de pasos espaciales
 
+// Definición computacional del sumidero de calor.
 double Gamma(double number);
 void F(double* Yin, double* Yout, int dim);
 void tridiag(double* a, double* b, double* c, double* r, double* u, int n);
@@ -53,19 +134,19 @@ int main(int argc, const char * argv[]) {
 
     printf("**********************************************\n");
     printf("Resolución de la ecuacion de calor u_t = k*u_xx - f \n");
-    printf("mediante el método de Runge-Kutta explícito\n");
+    printf("mediante el método de Crank-Nicolson implícito\n");
     printf("**********************************************\n");
 
 
 
     double u[nsteps][nnodes];
 
-    // Condición inicial (t=0)
+    // Condición inicial, t = 0
     for (int i = 0; i < nnodes; ++i) {
       u[0][i] = 0.0;
     }
 
-    // Condicion de borde (x=0; x=N)
+    // Condiciones de borde (x = 0; x = N)
     for (int i = 0; i < nsteps; ++i) {
           u[i][0] = T0;
           u[i][nnodes-1] = Tf;
@@ -73,7 +154,7 @@ int main(int argc, const char * argv[]) {
 
 
 
-    // Start allocating a, b, c and r vectors
+    // Implementación de los vectores input constituyentes del sistema de ecuaciones
     double *a = (double*)malloc(nnodes*sizeof(double));
     double *b = (double*)malloc(nnodes*sizeof(double));
     double *c = (double*)malloc(nnodes*sizeof(double));
@@ -86,19 +167,19 @@ int main(int argc, const char * argv[]) {
       b[i] = 1/dt - (a[i] + c[i]);
     }
 
-    // Arreglar para condiciones de borde
+    // Fijación de las condiciones de borde para garantizar matriz cuadrada de n x n
     b[0] = 1; b[nnodes-1] = 1;
     c[0] = 0; a[nnodes-1] = 0;
 
 
-    // IMPLICIT CRANK - NICOLSON
+    // IMPLEMENTACIÓN -- MÉTODO CRANK-NICOLSON
 
     for (int t = 0; t < nsteps-1; ++t) {
       r[0] = T0;
       for (int x = 1; x < nnodes-1; ++x) {
-        r[x] = - a[x]*u[t][x-1] + 
-                (1.0/dt + c[x] + a[x])*u[t][x] 
-               - c[x]*u[t][x+1] 
+        r[x] = - a[x]*u[t][x-1] +
+                (1.0/dt + c[x] + a[x])*u[t][x]
+               - c[x]*u[t][x+1]
                - Gamma(x*dx);
       }
       r[nnodes-1] = Tf;
@@ -108,16 +189,17 @@ int main(int argc, const char * argv[]) {
       tridiag(a, b, c, r, u[t+1], nnodes);
     }
 
-    // Guardamos los resultados en un archivo para su analisis posterior.
+    // Registro y retención de los datos obtenidos mediante el método numérico
+    // para el posterior análisis y uso gráfico animado.
     FILE *fp = fopen("res_implicit.csv", "w");
-    
+
     fprintf(fp, "t,");
     for (int i = 0; i < nnodes-1; ++i) {
         fprintf(fp, "x=%2.3f,", i*dx);
     }
     fprintf(fp, "x=%2.3f\n", dx*(nnodes-1));
 
-    
+
     for (int t=0; t<nsteps; t++) {
 
         fprintf(fp, "%3d,", t);
@@ -128,7 +210,7 @@ int main(int argc, const char * argv[]) {
         fprintf(fp, "%10.5f\n", u[t][nnodes-1]);
     }
 
-   // Imprimimos los valores finales como muestra.
+   // Exhibición de los valores finales como muestra de la efectividad del código
     printf("Simulación terminada exitosamente\n");
 
     free(fp);
@@ -136,19 +218,20 @@ int main(int argc, const char * argv[]) {
     free(b);
     free(c);
     free(r);
-    
+
     return 0;
 }
 
-  
 
+// Función sumidero-fuente en L/2 para la barra del calor descrita por la ecuación
+// del calor no homogénea.
 double Gamma(double number) {
-  /* Sumidero/fuente en L/2 para la ecuación de calor inhomogénea */
   return Theta / l * exp(-1.0 * pow((number - L/2)/l, 2));
 }
 
+// Resolución de los n sistemas de ecuaciones lineales que alberga la matriz.
 void F(double* Yin, double* Yout, int dim) {
-    Yout[0] = 0.0; Yout[dim-1] = 0.0;  // Truco para que las condiciones de borde funcionen
+    Yout[0] = 0.0; Yout[dim-1] = 0.0;     // Truco para que las condiciones de borde funcionen
     for (int i = 1; i < dim-1; ++i) {
         Yout[i] = kappa/(dx*dx) * (Yin[i-1] - 2*Yin[i] + Yin[i+1]) - Gamma(i*dx);
     }
@@ -166,12 +249,12 @@ void tridiag(double* a, double* b, double* c, double* r, double* u, int n)
    |            ...   0     a_n   b_n |   | u_n |   | r_n |
 
    a[1..n], b[1..n], c[1..n] y r[1..n] son vectores de input y no son
-   modificados. */
+   modificados.
+*/
+
 {
   int j;
   double bet, *gam;
-
-
 
   gam = (double*)malloc(n*sizeof(double));
   u[0] = r[0] / (bet=b[1]);
